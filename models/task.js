82f3ad20e19a02da6ecdda70c468726e76790174
Iztok Ramovš <iztok.ramovs@gmail.com>
2019-04-10 15:10:10 +0200

    Spaces again

---
+++ models/task.js
@@ -0,0 +1,64 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var Task = sequelize.define('Task', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        timeEstimate: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                min: {
+                    args: [0],
+                    msg: 'Cant be a negative value.'
+                }
+            }
+        },
+        createdAt: {
+            field: 'createdat',
+            type: DataTypes.DATE,
+        },
+        updatedAt: {
+            field: 'updatedat',
+            type: DataTypes.DATE,
+        },
+    },
+    {
+        indexes: [
+            {
+                unique: {
+                    args: true,
+                    msg: 'Task name already exists!'
+                },
+                fields: ['name', 'story_id']
+            }
+        ]
+    }
+
+    );
+
+    Task.associate = function (models) {
+        models.Task.belongsTo(models.Stories, {
+            onDelete: "CASCADE",
+            foreignKey: 'story_id',
+            as: 'Stories'
+        });
+        models.Task.belongsTo(models.User, {
+            onDelete: "CASCADE",
+            foreignKey: 'user_id',
+            as: 'User'
+        });
+    };
+
+    return Task;
+};
